f55bf2638d15bef04b73dd2c5d79a62946b1c1b1
666keke <jihaoran0509@163.com>
2022-11-18 23:06:36 +0800

    Update O.c
    Update: Better Format

--- O.c
+++ O.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int n, m, K, Q, lux, luy, rdx, rdy, x, y;
     int map[305][305] = {0}, last[305][305];
-    scanf("%d%d%d%d",&n,&m,&K,&Q);
+    scanf("%d%d%d%d", &n, &m, &K, &Q);
     for (int i = 0; i < K; ++i) {
-        scanf("%d%d%d%d",&lux,&luy,&rdx,&rdy);
+        scanf("%d%d%d%d", &lux, &luy, &rdx, &rdy);
         for (int j = lux; j <= rdx; ++j) {
             for (int k = luy; k <= rdy; ++k) {
                 map[j][k]++;
-                last[j][k] = i+1;
+                last[j][k] = i + 1;
             }
         }
     }
     for (int i = 0; i < Q; ++i) {
-        scanf("%d%d",&x,&y);
-        if(map[x][y]>0) printf("Y %d %d\n",map[x][y],last[x][y]);
+        scanf("%d%d", &x, &y);
+        if (map[x][y] > 0) printf("Y %d %d\n", map[x][y], last[x][y]);
         else printf("N\n");
     }
 }
